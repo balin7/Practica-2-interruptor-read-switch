f1a21098367870d28cee19c7664e10766de94e16
Oswaldo Araiza Diaz <36449700+balin7@users.noreply.github.com>
2018-03-09 13:34:37 -0600

    Add files via upload

---
+++ interruptor_magnetico.ino
@@ -0,0 +1,23 @@
+
+
+
+//Reed Switch y Arduino
+
+
+
+const int pinSensor = 2;
+const int pinLED = 13;
+void setup() {
+//configurar pin como entrada con resistencia pull-up interna
+pinMode(pinSensor, INPUT_PULLUP);
+pinMode(pinLED, OUTPUT);
+}
+void loop() {
+int value = digitalRead(pinSensor);
+if (value == LOW) {
+digitalWrite(pinLED, HIGH);
+} else {
+digitalWrite(pinLED, LOW);
+}
+delay(1000);
+}
